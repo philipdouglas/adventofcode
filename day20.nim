e1868ef3a6bf3717dff2a67cd0bc592150911d9c
Philip Douglas <psd@pdouglas.net>
2018-12-20 10:17:32 +0000

    Day 20: Refactor and tidy up

--- day20.nim
+++ day20.nim
@@ -13,96 +13,40 @@ import coord
 let input = input(day=20, year=2018)
 
 
+template move(direction: untyped) =
+    let newPos = pos.direction
+    if newPos notin rooms:
+        rooms[pos].add(newPos)
+        rooms[newPos] = @[]
+        depths[newPos] = depths[pos] + 1
+    stack.add(newPos)
+
+
 proc furthest(regex: string, debug: bool = false): tuple[part1: int, part2: int] =
-    # var
-    #     branches: seq[seq[seq[Coord]]] = @[@[@[[0, 0]]]]
-    # for cha in regex:
-    #     let pos = branches[^1][^1][^1]
-    #     case cha:
-    #         of '^': continue
-    #         of '$': return branches[0][0]
-    #         of '(': branches.add(@[@[@[pos]]])
-    #         of ')':
-    #             let fin = branches.pop().max
-    #             branches[^1][^1] += fin
-    #         of '|': branches[^1].add(@[0])
-    #         else: branches[^1][^1].inc
-    #     if debug: pause(&"{cha}: {branches}")
     var
         rooms = initTable[Coord, seq[Coord]]()
-        stack = @[[0, 0]]
         depths = initTable[Coord, int]()
+        stack = @[[0, 0]]
     rooms[[0, 0]] = @[]
     depths[[0, 0]] = 0
     for cha in regex:
-        let
-            pos = stack.pop()
-            depth = depths[pos]
+        let pos = stack.pop()
         case cha:
             of '^': stack.add(pos)
             of '$': break
-            of '(':
-                stack.add(pos)
-                stack.add(pos)
+            of '(': stack.add(@[pos, pos])
             of ')': discard
-            of '|':
-                stack.add(stack[^1])
-            of 'N':
-                let newPos = pos.up
-                if newPos notin rooms:
-                    rooms[pos].add(newPos)
-                    rooms[newPos] = @[]
-                    depths[newPos] = depth + 1
-                stack.add(newPos)
-            of 'E':
-                let newPos = pos.right
-                if newPos notin rooms:
-                    rooms[pos].add(newPos)
-                    rooms[newPos] = @[]
-                    depths[newPos] = depth + 1
-                stack.add(newPos)
-            of 'S':
-                let newPos = pos.down
-                if newPos notin rooms:
-                    rooms[pos].add(newPos)
-                    rooms[newPos] = @[]
-                    depths[newPos] = depth + 1
-                stack.add(newPos)
-            of 'W':
-                let newPos = pos.left
-                if newPos notin rooms:
-                    rooms[pos].add(newPos)
-                    rooms[newPos] = @[]
-                    depths[newPos] = depth + 1
-                stack.add(newPos)
+            of '|': stack.add(stack[^1])
+            of 'N': move(up)
+            of 'E': move(right)
+            of 'S': move(down)
+            of 'W': move(left)
             else: echo "Error!"
         if debug: pause(&"{cha}: {stack} {rooms}")
-    if debug: echo rooms
 
-    # var
-    #     open = initQueue[Coord]()
-    #     closed = initSet[Coord]()
-    #     depths = initTable[Coord, int]()
-    #     deepest = 0
-    #     # deepestPos = [0, 0]
-    # open.enqueue([0, 0])
-    # depths[[0, 0]] = 0
-    # while open.len > 0:
-    #     let
-    #         curr = open.dequeue()
-    #         depth = depths[curr]
-    #     if depth > deepest:
-    #         deepest = depth
-    #         # deepestPos = curr
-    #     for child in rooms[curr]:
-    #         if child notin closed:
-    #             open.add(child)
-    #             depths[child] = depth + 1
-    #     closed.incl(curr)
-    # return deepest
-    result.part1 = toSeq(depths.values).max
-    result.part2 = toSeq(depths.values).filterIt(it >= 1000).len
-
+    let finalDepths = toSeq(depths.values)
+    result.part1 = finalDepths.max
+    result.part2 = finalDepths.filterIt(it >= 1000).len
 
 
 check:
