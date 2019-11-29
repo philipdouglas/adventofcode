87c145787a868a55a1cbb1b177340f5b3a8cbd13
Philip Douglas <psd@pdouglas.net>
2019-11-29 18:41:49 +0000

    Add some aborted prep to 2019prep

---
+++ 2019prep/2018_d01.exs
@@ -0,0 +1,39 @@
+defmodule Day1 do
+  def part2(changes) do
+    changes
+    |> Stream.cycle
+    |> Stream.scan(fn(change, freq) -> freq + change end)
+    |> Enum.reduce_while(MapSet.new([0]), fn (freq, seen) ->
+      if MapSet.member?(seen, freq) do
+        {:halt, freq}
+      else
+        {:cont, MapSet.put(seen, freq)}
+      end
+    end)
+  end
+end
+
+ExUnit.start
+
+defmodule Day1Test do
+  use ExUnit.Case
+
+  test "part2" do
+    assert Day1.part2([1, -2, 3, 1]) == 2
+    assert Day1.part2([+1, -1]) == 0
+    assert Day1.part2([+3, +3, +4, -2, -4]) == 10
+    assert Day1.part2([-6, +3, +8, +5, -6]) == 5
+    assert Day1.part2([+7, +7, -2, -7, -4]) == 14
+  end
+end
+
+input = with {:ok, raw} = AdventOfCodeHelper.get_input(2018, 1), do: raw
+|> String.trim()
+|> String.split("\n")
+|> Enum.map(&String.to_integer/1)
+
+input |> Enum.sum |> IO.puts
+
+input
+|> Day1.part2
+|> IO.puts()
