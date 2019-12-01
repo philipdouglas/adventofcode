ea24ed5598d57bf1d10462fff4ad911370871355
Philip Douglas <psd@pdouglas.net>
2019-12-01 09:06:59 +0000

    Day 1: Part 2 ⭐

--- day01.py
+++ day01.py
@@ -19,11 +19,24 @@ def part1(modules):
     return sum(map(fuel_required, modules))
 
 
+def fuel_required_extended(mass):
+    """
+    >>> fuel_required_extended(14)
+    2
+    >>> fuel_required_extended(1969)
+    966
+    >>> fuel_required_extended(100756)
+    50346
+    """
+    total = 0
+    while (fuel := fuel_required(mass)) >= 0:
+        total += fuel
+        mass = fuel
+    return total
 
-# def part2(boxes):
-#     """
-#     >>> part2()
-#     """
+
+def part2(modules):
+    return sum(map(fuel_required_extended, modules))
 
 
 if __name__ == "__main__":
@@ -31,6 +44,6 @@ if __name__ == "__main__":
     doctest.testmod()
 
     puzzle = Puzzle(year=2019, day=1)
-    modules = map(int, puzzle.input_data.split('\n'))
+    modules = [int(module) for module in puzzle.input_data.split('\n')]
     puzzle.answer_a = part1(modules)
-    # puzzle.answer_b = part2(modules)
+    puzzle.answer_b = part2(modules)
