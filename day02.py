85886d9e5f9fcee721b28b8579bbbc78ef5f8976
Philip Douglas <psd@pdouglas.net>
2019-12-02 07:45:34 +0000

    Day 2: Part 2 ⭐

--- day02.py
+++ day02.py
@@ -1,10 +1,15 @@
 import copy
 import dataclasses
+import itertools
 from operator import add, mul
 
 from aocd.models import Puzzle
 
 from util import inspect
+
+
+class Halt(Exception):
+    pass
 
 
 @dataclasses.dataclass()
@@ -21,41 +26,50 @@ class Computer:
     >>> Computer([1, 1, 1, 4, 99, 5, 6, 0, 99]).run()
     [30, 1, 1, 4, 2, 5, 6, 0, 99]
     """
-    state: list
+    mem: list
     pc: int = 0
 
     def run(self):
-        while (last := self.execute()) != 99:
-            self.pc += 4
-        return self.state
+        try:
+            while True:
+                self.execute()
+        except Halt:
+            return self.mem
 
     def execute(self):
-        opcode = self.state[self.pc]
-        if opcode != 99:
-            if opcode == 1:
-                op = add
-            elif opcode == 2:
-                op = mul
-            else:
-                raise Exception(f"Unrecognised opcode {opcode} at pc {self.pc}")
-            input1, input2, output = self.state[self.pc + 1:self.pc + 4]
-            val1 = self.state[input1]
-            val2 = self.state[input2]
-            self.state[output] = op(val1, val2)
-        return opcode
+        opcode = self.mem[self.pc]
+        if opcode == 99:
+            raise Halt()
+        elif opcode == 1:
+            op = add
+        elif opcode == 2:
+            op = mul
+        else:
+            raise Exception(f"Unrecognised opcode {opcode} at pc {self.pc}")
+        input1, input2, output = self.mem[self.pc + 1:self.pc + 4]
+        val1 = self.mem[input1]
+        val2 = self.mem[input2]
+        self.mem[output] = op(val1, val2)
+        self.pc += 4
 
 
 def part1(state):
     comp = Computer(state)
     comp.run()
-    return comp.state[0]
+    return comp.mem[0]
 
 
-# def part2(lines):
-#     """
-#     >>> part2()
-#
-#     """
+def part2(state):
+    TARGET = 19690720
+    for noun, verb in itertools.product(range(100), range(100)):
+        new_state = copy.copy(state)
+        new_state[1] = noun
+        new_state[2] = verb
+        comp = Computer(new_state)
+        comp.run()
+        # print(f"{noun}, {verb}: {comp.mem[0]}")
+        if comp.mem[0] == TARGET:
+            return 100 * noun + verb
 
 
 if __name__ == "__main__":
@@ -68,4 +82,4 @@ if __name__ == "__main__":
     state_1[1] = 12
     state_1[2] = 2
     puzzle.answer_a = inspect(part1(state_1), prefix='Part 1: ')
-    # puzzle.answer_b = inspect(part2(start_state), prefix='Part 2: ')
+    puzzle.answer_b = inspect(part2(start_state), prefix='Part 2: ')
