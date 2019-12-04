f67772882bf04750bd3aa22f89dd07439c0cdaec
Philip Douglas <psd@pdouglas.net>
2019-12-04 07:29:19 +0000

    Day 4: Part 1 ⭐

---
+++ day04.py
@@ -0,0 +1,38 @@
+from aocd.models import Puzzle
+
+from util import inspect
+
+
+def check(password):
+    """
+    >>> check(111111)
+    True
+    >>> check(223450)
+    False
+    >>> check(123789)
+    False
+    """
+    password = str(password)
+    return password == ''.join(sorted(password)) and len(password) > len(set(password))
+
+
+def part1(low, high):
+    return len([password for password in range(low, high + 1) if check(password)])
+
+
+
+# def part2(lines):
+#     """
+#     >>> part2()
+#
+#     """
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+
+    puzzle = Puzzle(year=2019, day=4)
+    low, high = [int(bound) for bound in puzzle.input_data.split('-')]
+    puzzle.answer_a = inspect(part1(low, high), prefix='Part 1: ')
+    # puzzle.answer_b = inspect(part2(low, high), prefix='Part 2: ')
