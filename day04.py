115787dba8d34fc2627c170bbbd86318ac49271f
Philip Douglas <psd@pdouglas.net>
2019-12-04 07:29:46 +0000

    Day 4: Part 2 ⭐

--- day04.py
+++ day04.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 from aocd.models import Puzzle
 
 from util import inspect
@@ -20,12 +22,21 @@ def part1(low, high):
     return len([password for password in range(low, high + 1) if check(password)])
 
 
+def check2(password):
+    """
+    >>> check2(112233)
+    True
+    >>> check2(123444)
+    False
+    >>> check2(111122)
+    True
+    """
+    password = str(password)
+    return password == ''.join(sorted(password)) and 2 in Counter(password).values()
 
-# def part2(lines):
-#     """
-#     >>> part2()
-#
-#     """
+
+def part2(low, high):
+    return len([password for password in range(low, high + 1) if check2(password)])
 
 
 if __name__ == "__main__":
@@ -35,4 +46,4 @@ if __name__ == "__main__":
     puzzle = Puzzle(year=2019, day=4)
     low, high = [int(bound) for bound in puzzle.input_data.split('-')]
     puzzle.answer_a = inspect(part1(low, high), prefix='Part 1: ')
-    # puzzle.answer_b = inspect(part2(low, high), prefix='Part 2: ')
+    puzzle.answer_b = inspect(part2(low, high), prefix='Part 2: ')
