a26e57c3c8474cbce735935568be388e95b48c6b
Philip Douglas <psd@pdouglas.net>
2019-12-05 20:26:18 +0000

    Move Computer into a shared module

--- day02.py
+++ day02.py
@@ -1,67 +1,9 @@
-import dataclasses
 import itertools
-import operator
-from typing import List
 
 from aocd.models import Puzzle
 
+from computer import Computer
 from util import inspect
-
-
-class Halt(Exception):
-    pass
-
-
-@dataclasses.dataclass()
-class Computer:
-    """
-    >>> Computer([1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50]).run().mem
-    [3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50]
-    >>> Computer([1, 0, 0, 0, 99]).run().mem
-    [2, 0, 0, 0, 99]
-    >>> Computer([2, 3, 0, 3, 99]).run().mem
-    [2, 3, 0, 6, 99]
-    >>> Computer([2, 4, 4, 5, 99, 0]).run().mem
-    [2, 4, 4, 5, 99, 9801]
-    >>> Computer([1, 1, 1, 4, 99, 5, 6, 0, 99]).run().mem
-    [30, 1, 1, 4, 2, 5, 6, 0, 99]
-    """
-    mem: List[int]
-    pc: int = 0
-
-    _opcodes = {
-        1: operator.add,
-        2: operator.mul,
-    }
-
-    def __post_init__(self):
-        self.mem = self.mem.copy()
-
-    def run(self, noun=None, verb=None):
-        if noun is not None:
-            self[1] = noun
-        if verb is not None:
-            self[2] = verb
-
-        while (opcode := self.mem[self.pc]) != 99:
-            try:
-                op = self._opcodes[opcode]
-            except KeyError:
-                raise Exception(f"Unknown opcode {opcode} at pc {self.pc}")
-            input1, input2, output = self.mem[self.pc + 1:self.pc + 4]
-            self[output] = op(self[input1], self[input2])
-            self.pc += 4
-        return self
-
-    def __getitem__(self, key):
-        return self.mem[key]
-
-    def __setitem__(self, key, value):
-        self.mem[key] = value
-
-    @property
-    def output(self):
-        return self[0]
 
 
 def part1(state):
