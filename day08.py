4eeb6aa2fd281ea6ddbb9784a5d8761f7bcca10d
Philip Douglas <freakydug@gmail.com>
2019-12-08 08:41:02 +0000

    Day 8: Part 2 ⭐

--- day08.py
+++ day08.py
@@ -18,11 +18,19 @@ def part1(pixels):
     return fewest_zeroes[1] * fewest_zeroes[2]
 
 
-# def part2(pixels):
-#     """
-#     >>> part2()
-#
-#     """
+def part2(pixels):
+    layers = []
+    for start in range(0, len(pixels), LAYER_LENGTH):
+        layers.append(pixels[start:start + LAYER_LENGTH])
+    rows = []
+    for y in range(HEIGHT):
+        row = []
+        for x in range(WIDTH):
+            pixels = [layer[WIDTH * y + x] for layer in layers]
+            pixel = [pixel for pixel in pixels if pixel != 2][0]
+            row.append(' ' if pixel == 0 else '█')
+        rows.append(''.join(row))
+    return input('\n'.join(rows))
 
 
 if __name__ == "__main__":
@@ -32,4 +40,4 @@ if __name__ == "__main__":
     puzzle = Puzzle(year=2019, day=8)
     pixels = [int(pixel) for pixel in puzzle.input_data]
     puzzle.answer_a = inspect(part1(pixels), prefix='Part 1: ')
-    # puzzle.answer_b = inspect(part2(pixels), prefix='Part 2: ')
+    puzzle.answer_b = inspect(part2(pixels), prefix='Part 2: ')
